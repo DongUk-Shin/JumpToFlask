74a52f2750584671ebdfbd665d7f74e3ad9c170d
DongUk-Shin <az9170@naver.com>
2023-10-24 18:02:14 +0900

    부트스트랩 적용

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
-from pybo.models import Question
+from flask import Blueprint, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
+
 
 @bp.route('/hello')
 def hello_pybo():
@@ -10,4 +11,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'asdsaddsa'
+    return redirect(url_for('question._list'))
